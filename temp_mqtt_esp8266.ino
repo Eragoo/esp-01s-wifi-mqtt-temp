1c22217dc36a685c93e1a5531ca19e78b0860e15
Yevhenii Kukhol <37846731+Eragoo@users.noreply.github.com>
2020-12-31 18:17:28 +0200

    added sleep mode

--- temp_mqtt_esp8266.ino
+++ temp_mqtt_esp8266.ino
@@ -1,22 +1,3 @@
-/*
- Basic ESP8266 MQTT example
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-*/
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
@@ -24,16 +5,14 @@
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
-// Update these with values suitable for your network.
+const char* ssid = "";
+const char* password = "";
 
-const char* ssid = "I'm_teapot";
-const char* password = "kukhol2000";
-
-const char* mqtt_server = "192.168.31.134";
-const char* mqttUsername = "Erag0";
-const char* mqttPassword = "366326434";
-const char* mqttClientName = "Erag0";
-const char* tempTopic = "temp";
+const char* mqtt_server = "";
+const char* mqttUsername = "";
+const char* mqttPassword = "";
+const char* mqttClientName = "";
+const char* tempTopic = "";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -49,7 +28,6 @@ void setup() {
   
   
   delay(10);
-  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -83,7 +61,6 @@ void setup() {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
